cfc349f989c1677a3157549aff4e7386c6c10763
Pedro Renato <pedroprochnow6@gmail.com>
2026-03-19 22:12:43 -0300

    Create Exercicio03.py

---
+++ Semana03/Exercicio03.py
@@ -0,0 +1,6 @@
+preco_litro = float(input("Digite o preço do litro: R$"))
+valor_pagamento = float(input("Digite o valor que deseja abastecer: R$"))
+litros = valor_pagamento / preco_litro
+
+
+print("Quantidade de litros abastecidos:", litros, "L")
