be885bb25ad376d8382f95b1b3a546d4d299f486
Pedro Renato <pedroprochnow6@gmail.com>
2026-03-26 22:01:54 -0300

    Create Exercicio2.py

---
+++ Semana04/Exercicio2.py
@@ -0,0 +1,15 @@
+letra = (input("Digite a sua letra: "))
+
+if letra == "a":
+    print("Vogal")
+elif letra == "e":
+    print("Vogal")
+elif letra == "i":
+    print("Vogal")
+elif letra == "o":
+    print("Vogal")
+elif letra == "u":
+    print("Vogal")
+else:
+    print("Consoante")
+  
