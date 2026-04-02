92d838699319c18c78d7c7d185da302ad7087bba
Pedro Renato <pedroprochnow6@gmail.com>
2026-04-02 18:05:23 -0300

    Create Exercicio3.py

---
+++ Semana04/Exercicio3.py
@@ -0,0 +1,10 @@
+pontuacao_time1 = int(input("Grêmio marcou quantos pontos:"))
+pontuacao_time2 = int(input("Internacional marcou quantos pontos: "))
+
+if pontuacao_time1 > pontuacao_time2:
+     print("Grêmio ganhou")
+elif pontuacao_time2 > pontuacao_time1:
+     print("Internacional ganhou")
+else:
+     print("Empate")
+  
