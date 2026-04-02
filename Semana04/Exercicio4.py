4f9805823f36539566432a5cd0647a7dd9469342
Pedro Renato <pedroprochnow6@gmail.com>
2026-04-02 18:26:58 -0300

    Update Exercicio4.py

--- Semana04/Exercicio4.py
+++ Semana04/Exercicio4.py
@@ -1,1 +1,13 @@
+valor = float(input("Digite o valor: "))
+opcao = int(input("1-à vista, 2-2x, 3-3x: "))
 
+if opcao == 1:
+    print("Pagamento à vista: R$", valor)
+elif opcao == 2:
+    parcela = valor / 2
+    print("2 parcelas de: R$", parcela)
+elif opcao == 3:
+    parcela = valor / 3
+    print("3 parcelas de: R$", parcela)
+else:
+    print("Opção invalida")
