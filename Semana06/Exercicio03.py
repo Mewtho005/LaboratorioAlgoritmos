b32c53d3ff3f1316935a829bd68e2ea6446dbfc9
Pedro Renato <pedroprochnow6@gmail.com>
2026-05-01 11:10:42 -0300

    Update Exercicio03.py

--- Semana06/Exercicio03.py
+++ Semana06/Exercicio03.py
@@ -1,10 +1,11 @@
 soma = 0
 menos30 = 0
 entre30e60 = 0
+i = 1
 
-for i in range(7):
-    tempo = float(input("Digite o tempo do corredor em minutos: "))
-    
+while i <= 7:
+    tempo = float(input("Digite o tempo do corredor: "))
+
     soma = soma + tempo
 
     if tempo < 30:
@@ -13,9 +14,11 @@ for i in range(7):
     if tempo >= 30 and tempo <= 60:
         entre30e60 = entre30e60 + 1
 
+    i = i + 1
+
 media = soma / 7
 porcentagem = (entre30e60 / 7) * 100
 
-print("Tempo médio dos corredores:", media)
-print("Corredores com menos de 30 minutos:", menos30)
+print("Tempo médio:", media)
+print("Menos de 30 minutos:", menos30)
 print("Porcentagem entre 30 e 60 minutos:", porcentagem, "%")
