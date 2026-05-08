400f319818e9dd766e0d38e605bce539f8d32e97
Pedro Renato <pedroprochnow6@gmail.com>
2026-05-07 21:09:59 -0300

    Create Exercicio02.py

---
+++ Semana10/Exercicio02.py
@@ -0,0 +1,11 @@
+contatador = 0
+variavel = 0
+amais = 0
+
+for contatador in range(1, 11):
+    variavel = int(input("Digite o numero: "))
+    amais = amais + variavel
+
+variavel = amais / 10
+
+print(variavel)
