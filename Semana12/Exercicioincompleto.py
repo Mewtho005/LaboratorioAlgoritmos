18e3ba52891e802254c00ea42fffbbda8d2ee811
Pedro Renato <pedroprochnow6@gmail.com>
2026-05-21 22:25:42 -0300

    Create Exercicioincompleto.py

---
+++ Semana12/Exercicioincompleto.py
@@ -0,0 +1,22 @@
+#Utilize duas funções para realizar os cálculos
+
+
+def numero():
+    n1 = float(input("Digite o primeira numero:"))
+    return n1
+
+def calculardobro(numero1):
+    dobro = (numero1) * 2
+    return dobro
+
+def calculartriplo(numero2):
+    triplo = (numero2) * 3
+
+
+
+
+def main():
+    numero1 = calculardobro()
+    numero2 = calculartriplo()
+
+main()
