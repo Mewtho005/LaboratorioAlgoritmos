6fc0b77d7636cf16db7af33f81cb4edb0faed414
Pedro Renato <pedroprochnow6@gmail.com>
2026-06-03 11:04:14 -0300

    Update Exercicioincompleto.py

--- Semana12/Exercicioincompleto.py
+++ Semana12/Exercicioincompleto.py
@@ -1,22 +1,24 @@
 #Utilize duas funções para realizar os cálculos
 
-
 def numero():
-    n1 = float(input("Digite o primeira numero:"))
+    n1 = float(input("Digite um numero: "))
     return n1
 
 def calculardobro(numero1):
-    dobro = (numero1) * 2
+    dobro = numero1 * 2
     return dobro
 
 def calculartriplo(numero2):
-    triplo = (numero2) * 3
-
-
-
+    triplo = numero2 * 3
+    return triplo
 
 def main():
-    numero1 = calculardobro()
-    numero2 = calculartriplo()
+    n = numero()
+    
+    dobro = calculardobro(n)
+    triplo = calculartriplo(n)
+    
+    print("Dobro:", dobro)
+    print("Triplo:", triplo)
 
 main()
